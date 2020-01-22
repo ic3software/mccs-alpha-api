0f4deb3d4f044d60ee0df6454ce4db3e273e6efc
LIYINGZHEN <maxlivinci@gmail.com>
2020-01-22 23:34:49 +0800

    improvment

--- internal/pkg/util/email.go
+++ internal/pkg/util/email.go
@@ -4,10 +4,16 @@ import "regexp"
 
 var emailRe = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-// IsValidEmail checks if email is valid.
 func IsValidEmail(email string) bool {
 	if email == "" || !emailRe.MatchString(email) || len(email) > 100 {
 		return false
 	}
 	return true
+}
+
+func IsInValidEmail(email string) bool {
+	if email == "" || !emailRe.MatchString(email) || len(email) > 100 {
+		return true
+	}
+	return false
 }
