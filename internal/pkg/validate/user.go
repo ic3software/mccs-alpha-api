38ecbcef81942f53beda90fc00535a8b0aa9e0de
LIYINGZHEN <maxlivinci@gmail.com>
2020-02-07 21:18:38 +0800

    update validation

--- internal/pkg/validate/user.go
+++ internal/pkg/validate/user.go
@@ -17,16 +17,13 @@ var (
 func SignUp(req types.SignupRequest) []error {
 	errs := []error{}
 
-	req.Email = strings.ToLower(req.Email)
-	if req.Email == "" {
-		errs = append(errs, errors.New("Email is missing."))
-	} else if len(req.Email) > emailMaxLen {
-		errs = append(errs, errors.New("Email address length cannot exceed "+strconv.Itoa(emailMaxLen)+" characters."))
-	} else if util.IsInValidEmail(req.Email) {
-		errs = append(errs, errors.New("Email is invalid."))
-	}
-
+	errs = append(errs, checkEmail(req.Email)...)
 	errs = append(errs, validatePassword(req.Password)...)
+	errs = append(errs, checkUser(types.User{
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		Telephone: req.UserPhone,
+	})...)
 
 	return errs
 }
@@ -48,15 +45,38 @@ func ResetPassword(password string) []error {
 func UpdateUser(update types.UpdateUser) []error {
 	errs := []error{}
 
-	if len(update.FirstName) > 100 {
+	errs = append(errs, checkUser(types.User{
+		FirstName: update.FirstName,
+		LastName:  update.LastName,
+		Telephone: update.UserPhone,
+	})...)
+
+	return errs
+}
+
+func checkEmail(email string) []error {
+	errs := []error{}
+	email = strings.ToLower(email)
+	if email == "" {
+		errs = append(errs, errors.New("Email is missing."))
+	} else if len(email) > emailMaxLen {
+		errs = append(errs, errors.New("Email address length cannot exceed "+strconv.Itoa(emailMaxLen)+" characters."))
+	} else if util.IsInValidEmail(email) {
+		errs = append(errs, errors.New("Email is invalid."))
+	}
+	return errs
+}
+
+func checkUser(user types.User) []error {
+	errs := []error{}
+	if len(user.FirstName) > 100 {
 		errs = append(errs, errors.New("First name length cannot exceed 100 characters."))
 	}
-	if len(update.LastName) > 100 {
+	if len(user.LastName) > 100 {
 		errs = append(errs, errors.New("Last name length cannot exceed 100 characters."))
 	}
-	if len(update.UserPhone) > 25 {
+	if len(user.Telephone) > 25 {
 		errs = append(errs, errors.New("Telephone length cannot exceed 25 characters."))
 	}
-
 	return errs
 }
