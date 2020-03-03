b2f1d3b2930d6f84682c243269afef59c39fa645
Geoff Turk <geoffturk27@gmail.com>
2020-03-03 11:03:48 +0100

    Update seed data and error messages

--- internal/app/http/controller/entity_handler.go
+++ internal/app/http/controller/entity_handler.go
@@ -214,11 +214,11 @@ func (handler *entityHandler) addToFavoriteEntities() func(http.ResponseWriter, 
 
 func (handler *entityHandler) checkEntityStatus(SenderEntity, ReceiverEntity *types.Entity) error {
 	if !util.IsTradingAccepted(SenderEntity.Status) {
-		return errors.New("Sender is not in the accepted status.")
+		return errors.New("Sender does not have the correct status.")
 
 	}
 	if !util.IsTradingAccepted(ReceiverEntity.Status) {
-		return errors.New("Receiver is not in the accepted status.")
+		return errors.New("Receiver does not have the correct status.")
 	}
 	return nil
 }
