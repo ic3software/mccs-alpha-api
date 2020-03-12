963d3268bc98f6cbe204ca4630ab29d143962aad
Max Li <maxlivinci@gmail.com>
2020-03-12 22:16:32 +0800

    fix: check status before updating an entity (#65)

--- util/entity_status.go
+++ util/entity_status.go
@@ -4,6 +4,18 @@ import (
 	"github.com/ic3network/mccs-alpha-api/global/constant"
 )
 
+func IsValidStatus(status string) bool {
+	if status == constant.Entity.Pending ||
+		status == constant.Entity.Rejected ||
+		status == constant.Entity.Accepted ||
+		status == constant.Trading.Pending ||
+		status == constant.Trading.Accepted ||
+		status == constant.Trading.Rejected {
+		return true
+	}
+	return false
+}
+
 // IsAcceptedStatus checks if the entity status is accpeted.
 func IsAcceptedStatus(status string) bool {
 	if status == constant.Entity.Accepted ||
