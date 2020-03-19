7e60d4f394c9b76c962b882c14afa971817554a1
Max Li <maxlivinci@gmail.com>
2020-03-19 18:07:27 +0800

    chore: set balance limits in seed script (#71)

--- internal/app/types/balance_limit.go
+++ internal/app/types/balance_limit.go
@@ -8,7 +8,7 @@ type BalanceLimit struct {
 	gorm.Model
 	// `BalanceLimit` belongs to `Account`, `AccountID` is the foreign key
 	Account   Account
-	AccountID uint    `gorm:"not null;unique_index"`
-	MaxNegBal float64 `gorm:"type:int;not null"`
-	MaxPosBal float64 `gorm:"type:int;not null"`
+	AccountID uint    `json:"accountID,omitempty" gorm:"not null;unique_index"`
+	MaxNegBal float64 `json:"maxNegBal,omitempty" gorm:"type:real;not null"`
+	MaxPosBal float64 `json:"maxPosBal,omitempty" gorm:"type:real;not null"`
 }
