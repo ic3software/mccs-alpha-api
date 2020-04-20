233f95f446beee7e6f23f14c65f5edf46c3c5291
LIYINGZHEN <maxlivinci@gmail.com>
2020-04-20 20:16:51 +0800

    allow get post patch and delete method

--- internal/app/http/middleware/cors.go
+++ internal/app/http/middleware/cors.go
@@ -11,6 +11,12 @@ func CORS() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			c := cors.New(cors.Options{
+				AllowedMethods: []string{
+					http.MethodGet,
+					http.MethodPost,
+					http.MethodPatch,
+					http.MethodDelete,
+				},
 				AllowCredentials: true,
 			})
 			c.HandlerFunc(w, r)
