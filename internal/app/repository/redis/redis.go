1bc3164fe61df4274487f19dbaaea2112c41b9a4
maxlivinci <maxlivinci@gmail.com>
2023-10-14 09:11:46 -0700

    chore(deps): update redis pacakge

--- internal/app/repository/redis/redis.go
+++ internal/app/repository/redis/redis.go
@@ -1,45 +1,67 @@
 package redis
 
 import (
+	"context"
 	"log"
 	"strconv"
 	"time"
 
-	"github.com/go-redis/redis"
-	"github.com/ic3network/mccs-alpha-api/global"
-	"github.com/ic3network/mccs-alpha-api/util/l"
+	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+
+	"github.com/ic3network/mccs-alpha-api/global"
+	"github.com/ic3network/mccs-alpha-api/util/l"
 )
 
-var client *redis.Client
+var (
+	ctx                  = context.Background()
+	client               *redis.Client
+	rateLimitingDuration time.Duration
+	loginAttemptsTimeout time.Duration
+)
 
 func init() {
 	global.Init()
 
+	// Get Redis configuration.
 	host := viper.GetString("redis.host")
 	port := viper.GetString("redis.port")
 	password := viper.GetString("redis.password")
+
+	// Get other configuration values,
+	rateLimitingDuration = viper.GetDuration(
+		"rate_limiting.duration",
+	) * time.Minute
+	loginAttemptsTimeout = viper.GetDuration(
+		"login_attempts.timeout",
+	) * time.Second
+
 	client = redis.NewClient(&redis.Options{
 		Addr:     host + ":" + port,
 		Password: password,
 		DB:       0, // use default DB
 	})
 
+	waitForConnection()
+}
+
+func waitForConnection() {
 	for {
-		_, err := client.Ping().Result()
+		_, err := client.Ping(ctx).Result()
 		if err != nil {
 			log.Printf("Redis connection error: %+v \n", err)
-			time.Sleep(5 * time.Second)
+			time.Sleep(3 * time.Second)
 		} else {
 			break
 		}
 	}
 }
 
+// GetRequestCount returns the request count for a given IP address.
 func GetRequestCount(ip string) int {
 	key := Ratelimiting + ":" + ip
-	val, err := client.Get(key).Result()
+	val, err := client.Get(ctx, key).Result()
 	if err != nil {
 		return 0
 	}
@@ -49,28 +71,26 @@ func GetRequestCount(ip string) int {
 
 func IncRequestCount(ip string) error {
 	key := Ratelimiting + ":" + ip
-
-	_, err := client.Get(key).Result()
+	_, err := client.Get(ctx, key).Result()
 	if err == redis.Nil {
-		err = client.Set(key, 1, viper.GetDuration("rate_limiting.duration")*time.Minute).Err()
-		if err != nil {
-			l.Logger.Error("[ERROR] redis IncRequestCount failed:", zap.Error(err))
-			return err
-		}
+		// If the key does not exist, set it with a value of 1
+		err = client.Set(
+			ctx, key, 1, rateLimitingDuration,
+		).Err()
 	} else {
-		err = client.Incr(key).Err()
-		if err != nil {
-			l.Logger.Error("[ERROR] redis IncRequestCount failed:", zap.Error(err))
-			return err
-		}
+		err = client.Incr(ctx, key).Err()
 	}
 
-	return nil
+	if err != nil {
+		l.Logger.Error("[ERROR] redis IncRequestCount failed:", zap.Error(err))
+	}
+	return err
 }
 
+// GetLoginAttempts returns the login attempts count for a given email address.
 func GetLoginAttempts(email string) int {
 	key := LoginAttempts + ":" + email
-	val, err := client.Get(key).Result()
+	val, err := client.Get(ctx, key).Result()
 	if err != nil {
 		return 0
 	}
@@ -80,29 +100,30 @@ func GetLoginAttempts(email string) int {
 
 func IncLoginAttempts(email string) error {
 	key := LoginAttempts + ":" + email
-
-	_, err := client.Get(key).Result()
+	_, err := client.Get(ctx, key).Result()
 	if err == redis.Nil {
-		err = client.Set(key, 1, viper.GetDuration("login_attempts.timeout")*time.Second).Err()
-		if err != nil {
-			l.Logger.Error("[ERROR] redis IncLoginAttempts failed:", zap.Error(err))
-			return err
-		}
+		// If the key does not exist, set it with a value of 1.
+		err = client.Set(
+			ctx, key, 1, loginAttemptsTimeout,
+		).Err()
 	} else {
-		err = client.Incr(key).Err()
-		if err != nil {
-			l.Logger.Error("[ERROR] redis IncLoginAttempts failed:", zap.Error(err))
-			return err
-		}
+		err = client.Incr(ctx, key).Err()
 	}
 
-	return nil
+	if err != nil {
+		l.Logger.Error("[ERROR] redis IncLoginAttempts failed:", zap.Error(err))
+	}
+	return err
 }
 
+// ResetLoginAttempts resets the login attempts count for a given email address.
 func ResetLoginAttempts(email string) {
 	key := LoginAttempts + ":" + email
-	_, err := client.Del(key).Result()
+	_, err := client.Del(ctx, key).Result()
 	if err != nil {
-		l.Logger.Error("[ERROR] redis ResetLoginAttempts failed:", zap.Error(err))
+		l.Logger.Error(
+			"[ERROR] redis ResetLoginAttempts failed:",
+			zap.Error(err),
+		)
 	}
 }
